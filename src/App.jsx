d240a450eb444768d218c1315c05f50ce6f34a3a
arnavkumar2k5 <144643023+arnavkumar2k5@users.noreply.github.com>
2024-10-10 22:57:27 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './Pages/Header'
-import Home from './Pages/Home'
-import Footer from './Pages/Footer'
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Header from './Pages/Header';
+import Home from './Pages/Home';
+import Footer from './Pages/Footer';
 
 function App() {
+  const [showContent, setShowContent] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowContent(true);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <>
-      <Header/>
-      <Home/>
+      {!showContent ? (
+        <div className="flex justify-center items-center h-screen bg-black">
+          <img src="load2.gif" alt="load"/>
+        </div>
+      ) : (
+        <>
+          <Header />
+          <Home />
+          <Footer />
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
