90e8b5989a8e6f253e496f7d9e71cb54f8b22027
arnavkumar2k5 <144643023+arnavkumar2k5@users.noreply.github.com>
2024-10-10 17:25:44 +0530

    first commit

---
+++ src/Pages/Footer.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-[#1A1A1E] text-gray-300 py-12 px-6 w-screen">
+      <div className="flex flex-col md:flex-row justify-between items-start gap-10">
+        
+        <div className="mb-8 md:mb-0 w-full md:w-2/6 pl-5 pt-5">
+          <h2 className="text-2xl md:text-3xl font-semibold text-start text-white">Arnav's Portfolio</h2>
+          <p className="mt-5 text-gray-400 m-auto text-xl text-justify md:text-2xl md:pr-32"> 
+            Thank you for visiting my portfolio. Let's connect on social media.
+          </p>
+          <p className="mt-5 text-gray-400 text-start text-xl md:text-2xl">
+            Keep rising 🚀. Connect with me over live chat!
+          </p>
+        </div>
+
+        <div className="mb-8 md:mb-0 w-full md:w-2/6 pl-5 pt-5">
+          <h3 className="text-2xl md:text-3xl font-semibold text-white text-start">Quick Links</h3>
+          <ul className="mt-5 space-y-2 text-start text-xl md:text-2xl">
+            <li>
+              <a href="/" className="hover:text-white transition-colors">
+                ➤ Home
+              </a>
+            </li>
+            <li>
+              <a href="/" className="hover:text-white transition-colors">
+                ➤ About
+              </a>
+            </li>
+            <li>
+              <a href="/" className="hover:text-white transition-colors">
+                ➤ Skills
+              </a>
+            </li>
+            <li>
+              <a href="/" className="hover:text-white transition-colors">
+                ➤ Education
+              </a>
+            </li>
+            <li>
+              <a href="/" className="hover:text-white transition-colors">
+                ➤ Work
+              </a>
+            </li>
+            <li>
+              <a href="/" className="hover:text-white transition-colors">
+                ➤ Experience
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        
+        <div className="w-full md:w-2/6">
+          <h3 className="text-2xl md:text-3xl font-semibold text-white text-start md:text-center">Contact Info</h3>
+          <div className="flex flex-col m-auto">
+            <a
+              href="mailto:arnavkumar2k5@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center space-x-4 p-4 transition-transform transform hover:scale-105"
+            >
+              <img src="/Contact/mail.png" alt="mail" className="h-10 md:h-16" />
+              <p className="md:text-lg text-white text-sm">arnavkumar2k5@gmail.com</p>
+            </a>
+            <a
+              href="https://github.com/arnavkumar2k5"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center space-x-4 p-4 transition-transform transform hover:scale-105"
+            >
+              <img src="/Contact/github.png" alt="github" className="h-10 md:h-16" />
+              <p className="md:text-lg text-white text-sm">GitHub</p>
+            </a>
+            <a
+              href="https://x.com/Arnav_2k5"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center space-x-4 p-4 transition-transform transform hover:scale-105"
+            >
+              <img src="/Contact/twitter.png" alt="twitter" className="h-10 md:h-16" />
+              <p className="md:text-lg text-white text-sm">X.com</p>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/arnav-kumar-55a0a6276/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center space-x-4 p-4 transition-transform transform hover:scale-105"
+            >
+              <img
+                src="/Contact/linkedin.png"
+                className="h-10 md:h-16"
+                alt="linkedin"
+              />
+              <p className="md:text-lg text-white text-sm">LinkedIn</p>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="mt-8 text-center text-xl text-gray-400">
+        <p>
+          Designed with ❤️ by <span className="text-orange-400">Arnav Kumar</span>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
+
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,118 @@
+import React from "react";
+import { Link } from "react-scroll";
+import Design from "../Components/particles-effect/Design";
+import Footer from "./Footer";
+import Contact from "../Components/Contact/Contact";
+import Projects from "../Components/Projects/Projects";
+import About from "../Components/About/About";
+import TypeEffect from "../Text/Text";
+
+function Home() {
+  return (
+    <div className="w-screen h-screen text-center text-3xl items-center relative">
+      <div
+        className="flex flex-col lg:flex-row md:ml-20 items-center min-h-screen px-10 lg:px-20 py-20 mt-20 justify-between"
+        id="/"
+      >
+        <div className="lg:w-1/2 text-center lg:text-left order-last lg:order-first z-10">
+          <h1 className="text-4xl lg:text-7xl font-bold mb-4 text-cyan-400">
+            Hi there!
+          </h1>
+          <h2 className="text-3xl lg:text-7xl font-semibold text-gray-200 md:mb-6">
+            I'm Arnav Kumar
+          </h2>
+          <div className=" bg-cyan-500 border-b border-gray-300 w-[90%] mx-auto md:mx-0 mt-5 mb-5"></div>
+          <div className="h-32 text-2xl w-screen md:text-4xl font-bold text-white">
+            <TypeEffect />
+          </div>
+          <div className="flex gap-5 mb-10 justify-center md:justify-start">
+            <div className="bg-yellow-400 hover:bg-orange-500 text-xl md:text-4xl p-2 md:p-5 rounded-full hover:cursor-pointer">
+              <Link to="about" smooth={true} duration={500}>
+                About Me
+              </Link>
+            </div>
+            <div className="bg-yellow-400 hover:bg-orange-500 text-xl md:text-4xl p-2 md:p-5 rounded-full hover:cursor-pointer">
+              <Link to="contact" smooth={true} duration={500}>
+                Contact
+              </Link>
+            </div>
+          </div>
+          <div className="flex gap-5 md:gap-10 justify-center md:justify-start">
+            <a
+              href="arnavkumar2k5@gmail.com"
+              target="_blank"
+              className="hover:cursor-pointer transition-transform transform hover:scale-110"
+            >
+              <img
+                src="/Contact/mail.png"
+                alt="mail"
+                className="h-12 md:h-20"
+              />
+            </a>
+            <a
+              href="https://github.com/arnavkumar2k5"
+              target="_blank"
+              className="hover:cursor-pointer transition-transform transform hover:scale-110"
+            >
+              <img
+                src="/Contact/github.png"
+                alt="github"
+                className="h-12 md:h-20"
+              />
+            </a>
+            <a
+              href="https://x.com/Arnav_2k5"
+              target="_blank"
+              className="hover:cursor-pointer transition-transform transform hover:scale-110"
+            >
+              <img
+                src="/Contact/twitter.png"
+                alt="twitter"
+                className="h-12 md:h-20"
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/arnav-kumar-55a0a6276/"
+              target="_blank"
+              className="hover:cursor-pointer transition-transform transform hover:scale-110"
+            >
+              <img
+                src="/Contact/linkedin.png"
+                className="h-12 md:h-20"
+                alt="linkedin"
+              />
+            </a>
+          </div>
+        </div>
+
+        <div className="md:bg-yellow-400 rounded-full m-auto shadow-xl order-first mb-10 md:m-0 lg:order-last z-10 transform transition-transform duration-300 hover:rotate-3 hover:scale-105">
+          <img
+            src="/profile/profile-2.png"
+            alt="profile"
+            className="md:h-[60vh] rounded-full shadow-lg w-full"
+          />
+        </div>
+      </div>
+
+      <div className="h-auto">
+        <About />
+      </div>
+
+      <div>
+        <Projects />
+      </div>
+      <div>
+        <Contact />
+      </div>
+      <div>
+        <Footer />
+      </div>
+
+      <div className="w-full h-screen absolute top-0 left-0">
+        <Design />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
